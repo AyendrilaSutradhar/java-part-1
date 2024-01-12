0a3fa5194b1502c1bfcd711c861dea4b4a8fc491
AyendrilaSutradhar <150955530+AyendrilaSutradhar@users.noreply.github.com>
2024-01-12 14:10:55 +0000

    1st tut done

---
+++ one.java
@@ -0,0 +1,29 @@
+/**
+ * one
+ */
+// public class one {
+
+//     public static void main(String[] args) {
+//         System.out.println("buluram");
+//     }
+// }
+
+
+// public class one {
+
+//     public static void main(String[] args) {
+//         System.out.print("buluram");
+//         System.out.print("buchuram");
+//         System.out.println("baburam");
+//     }
+// }
+
+
+public class one {
+
+    public static void main(String[] args) {
+        System.out.println("buluram");
+        System.out.print("buchuram");
+        System.out.println("baburam");
+    }
+}
